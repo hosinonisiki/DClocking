8fb78d1040955963fd5cae86e8a2369486b64969
hosinonisiki <hosinoseikatu@hotmail.com>
2025-03-12 19:15:31 +0800

    Little modification on testing script

---
+++ python control/port_numbers.py
@@ -0,0 +1,29 @@
+# Simply recording the corresponding port numbers of different modules as a temporary solution.
+
+# Output from module, input to signal router:
+TRI_SIN = 0
+TRI_COS = 1
+ACC_OUT = 4
+SCALER_OUT = 5
+INPUT_A = 6
+INPUT_B = 7
+INPUT_C = 8
+INPUT_D = 9
+MIM_OUT_A = 10
+MIM_OUT_B = 11
+MIM_OUT_C = 12
+MIM_OUT_D = 13
+PID_OUT = 14
+
+# Output from signal router, input to module:
+OUTPUT_A = 0
+OUTPUT_B = 1
+OUTPUT_C = 2
+OUTPUT_D = 3
+TRI_IN = 4
+SCALER_IN = 5
+MIM_IN_A = 6
+MIM_IN_B = 7
+MIM_IN_C = 8
+MIM_IN_D = 9
+PID_IN = 10
